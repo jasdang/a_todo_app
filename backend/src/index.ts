e76b92c1391f773b1b1a7b0de389578ef4492e23
jasmined09 <jasmined@anafore.com>
2021-06-06 16:40:27 +0800

    dockerize

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,14 @@
-import Koa from 'koa'
-import Router from 'koa-router'
+import Koa from "koa"
+import Router from "koa-router"
 const app = new Koa()
 const router = new Router()
 
-router.get('/', (ctx) => {
-  ctx.body = 'Backend running at port 5000'
+router.get("/", (ctx) => {
+  ctx.body = "Backend running at port 3001"
 })
 
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(5000, () => {
-  console.log('Server running at PORT 5000')
+app.listen(3001, () => {
+  console.log("Server running at PORT 3001")
 })
