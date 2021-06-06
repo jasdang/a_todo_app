5ba95d2365a193cdd09e1b6c1694b5aca35cee63
jasmined09 <jasmined@anafore.com>
2021-06-06 16:40:03 +0800

    added sql schema

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS tododb;
+USE tododb;
+
+CREATE TABLE todos (
+  id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255),
+  completed VARCHAR(255),
+  deadline VARCHAR(255),
+);
