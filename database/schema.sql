e583a80346b19c39ac661bd762d7ed8f14451458
jasmined09 <jasmined@anafore.com>
2021-08-09 16:46:40 +0800

    update schema

--- database/schema.sql
+++ database/schema.sql
@@ -2,8 +2,9 @@ CREATE DATABASE IF NOT EXISTS tododb;
 USE tododb;
 
 CREATE TABLE todos (
-  id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  id int(10) unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
   title VARCHAR(255),
-  completed VARCHAR(255),
-  deadline VARCHAR(255)
+  completed BOOLEAN,
+  deadline datetime,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
